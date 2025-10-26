e3e92f8f197add65dc632c90bd5ecf09d5b04374
valeriyalexeeva15-lgtm <lera.alekseeva.4240@mail.ru>
2025-10-26 18:12:18 +0300

    Задание 3

--- AlekseevaDZ6/Program.cs
+++ AlekseevaDZ6/Program.cs
@@ -2,45 +2,23 @@
 {
     internal class Program
     {
-        static double Add(double a, double b) => a + b;
-        static double Subtract(double a, double b) => a - b;
-        static double Multiply(double a, double b) => a * b;
-        static double Divide(double a, double b) => a / b;
-        static double Power(double a, double b) => Math.Pow(a, b);
+       
         static void Main(string[] args)
         {
-            while (true)
+           Console.Write("Введите делимое: ");
+            double dividend = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите делитель: ");
+            double divisor = Convert.ToDouble(Console.ReadLine());
+
+            while (divisor == 0)
             {
-                Console.WriteLine("\n1. Сложение");
-                Console.WriteLine("2. Вычитание");
-                Console.WriteLine("3. Умножение");
-                Console.WriteLine("4. Деление");
-                Console.WriteLine("5. Возведение в степень");
-                Console.WriteLine("exit - выход");
-                Console.Write("Выберите операцию: ");
-
-                string choice = Console.ReadLine() ?? "";
-                if (choice.ToLower() == "exit") break;
-
-                Console.Write("Введите первое число: ");
-                double num1 = double.Parse(Console.ReadLine() ?? "0");
-                Console.Write("Введите второе число: ");
-                double num2 = double.Parse(Console.ReadLine() ?? "0");
-
-                double result = choice switch
-                {
-                    "1" => Add(num1, num2),
-                    "2" => Subtract(num1, num2),
-                    "3" => Multiply(num1, num2),
-                    "4" => Divide(num1, num2),
-                    "5" => Power(num1, num2),
-                    _ => 0
-                };
-
-                Console.WriteLine($"Результат: {result}");
+                Console.Write("На ноль делить нельзя. Повторите ввод делителя: ");
+                divisor = Convert.ToDouble(Console.ReadLine());
             }
-            Console.WriteLine("Калькулятор завершил работу!");
+
+            double result = dividend / divisor;
+            Console.WriteLine($"Результат: {result}");
         }
     }
 }
-
